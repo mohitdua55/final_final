8d14af8c0cee38cdedfb7ae98f0d6e3d4dff1b9d
mohitdua55 <mohitdua2001@gmail.com>
2024-10-16 16:47:53 +0530

    readme update

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -13,19 +13,3 @@ const TaskCard = ({ task }) => {
 };
 
 export default TaskCard;
-=======
-import React from "react";
-
-const TaskCard = ({ task }) => {
-  return (
-    <div className="task-card">
-      <h3>
-        {task.id}: {task.title}
-      </h3>
-      <p>Priority: {task.priority}</p>
-    </div>
-  );
-};
-
-export default TaskCard;
->>>>>>> b1c19640df77ec046fe8cdfaa1b73bf2d2929e16
